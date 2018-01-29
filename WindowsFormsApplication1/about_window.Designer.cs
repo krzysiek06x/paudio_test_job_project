ff46fa116800a5dc20d77c06e99f41dbbfa0314c
Krzysiek06x-kompJacky <krzysiek06x@gmail.com>
2018-01-29 19:36:43 +0100

    - Everything working - Finished

--- WindowsFormsApplication1/about_window.Designer.cs
+++ WindowsFormsApplication1/about_window.Designer.cs
@@ -32,6 +32,13 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,9 +50,10 @@
             this.ok_button.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ok_button.ForeColor = System.Drawing.SystemColors.Window;
             this.ok_button.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.ok_button.Location = new System.Drawing.Point(12, 403);
+            this.ok_button.Location = new System.Drawing.Point(13, 560);
+            this.ok_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ok_button.Name = "ok_button";
-            this.ok_button.Size = new System.Drawing.Size(572, 93);
+            this.ok_button.Size = new System.Drawing.Size(763, 114);
             this.ok_button.TabIndex = 5;
             this.ok_button.Text = "Ok";
             this.ok_button.UseVisualStyleBackColor = false;
@@ -54,7 +62,8 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.info_128;
-            this.pictureBox1.Location = new System.Drawing.Point(222, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(296, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(128, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -64,31 +73,111 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(193, 153);
+            this.label1.Location = new System.Drawing.Point(179, 185);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.Size = new System.Drawing.Size(245, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "Ten program, jest programem \"demo\"";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(193, 184);
+            this.label2.Location = new System.Drawing.Point(179, 223);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.Size = new System.Drawing.Size(214, 17);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Przysyłam go zgodnie z naszą rozmową. ";
+            this.label2.Text = "Działa on w następujący sposób:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 260);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(419, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Należy najpierw wyszukać porty, aby inne przyciski były dostępne";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(179, 296);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(349, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Następnie należy wybrać port i się pod niego dołączyć";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(179, 334);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(328, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Odblokowują się opcje do wybrania 1# lub 2# płytki";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(179, 374);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(414, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Następnie należałoby napisać funkcję które zaprogramują płytkę";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(179, 418);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(490, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Jednak jest to program \"demo\" więc takich funkcjonalności jeszcze nie ma :-)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(179, 460);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(326, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Po rozłączeniu się, automatycznie wyszukuje porty";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(179, 496);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(437, 17);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Wszystkie informację o tym co się dzieje jest napisane w dialogBoxie";
             // 
             // about_window
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(209)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(596, 508);
+            this.ClientSize = new System.Drawing.Size(795, 687);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ok_button);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "about_window";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "about_window";
@@ -104,5 +193,12 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
